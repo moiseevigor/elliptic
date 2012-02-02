9820b35061bb204f922e20fd5a0787338f9726fb
moiseev.igor <moiseev.igor@6c30bb40-4006-11de-b5fc-91815ed80701>
2012-02-02 20:55:09 +0000

    Inverseelliptic2 added controls

--- inverselliptic2.m
+++ inverselliptic2.m
@@ -1,15 +1,15 @@
 function invE = inverselliptic2(E,m,tol)
-% INVERSELLIPTIC evaluates the value of the INVERSE Incomplete Elliptic Integrals 
+% INVERSELLIPTIC2 evaluates the value of the INVERSE Incomplete Elliptic Integrals 
 % of the Second Kind.
 %
-%   INVE = INVERSELLIPTIC(E,M,TOL) where E is a value of the integral to 
-%   be converted, 0<M<1 is the module and TOL is the tolerance (optional). 
+%   INVE = INVERSELLIPTIC2(E,M,TOL) where E is a value of the integral to 
+%   be inverted, 0<M<1 is the module and TOL is the tolerance (optional). 
 %   Default value for the tolerance is eps = 2.220e-16.
 %
-%   INVERSELLIPTIC uses the method described by Boyd J. P. 
+%   INVERSELLIPTIC2 uses the method described by Boyd J. P. 
 %   to determine the value of the inverse Incomplete Elliptic Integrals 
 %   of the Second Kind using the “Empirical” initialization to 
-%   the Newton’s iteration method [1].
+%   the Newton’s iteration method [1]. 
 %
 %   NOTICE. Please pay attention to the definition of the elliptic functions
 %   which follows the Abramovitz et al [2], for more theory on elliptic
@@ -28,6 +28,17 @@ function invE = inverselliptic2(E,m,tol)
 %       r = sqrt((1-m)^2 + zeta^2)
 %       zeta = 1 - z/E(pi/2,m)
 %       theta = atan((1 - m)/zeta)
+%
+%
+%   Example:
+%       % modulus and phase in degrees
+%       [phi,alpha] = meshgrid(0:5:90, 0:2:90);
+%       % values of integrals
+%       [F,E] = elliptic12(pi/180*phi, sin(pi/180*alpha).^2);
+%       % values of inverse 
+%       invE = inverselliptic2(E, sin(pi/180*alpha).^2);
+%       % the difference between phase phi and invE should close to zero
+%       phi - invE * 180/pi
 %
 %   See also ELLIPKE, ELLIPTIC12.
 %
@@ -53,16 +64,41 @@ function invE = inverselliptic2(E,m,tol)
 %   ELLIPTIC PROJECT: http://elliptic.googlecode.com
 %   Group: 
 
-z=E; mu=1-m;
-[K,E1] = ellipke(m); 
+if nargin<3, tol = eps; end
+if nargin<2, error('Not enough input arguments.'); end
+
+if ~isreal(E) || ~isreal(m)
+    error('Input arguments must be real.');
+end
+
+if length(m)==1, m = m(ones(size(u))); end
+if length(E)==1, E = E(ones(size(m))); end
+if ~isequal(size(m),size(E)), error('E and M must be the same size.'); end
+
+invE = zeros(size(E)); 
+
+% make a row vector
+m = m(:);
+E = E(:);
+
+if any(m < 0) || any(m > 1), error('M must be in the range 0 <= M <= 1.'); end
+
+% inputs
+z = E; mu = 1-m;
+
+% complete integral initialization
+[~,E1] = ellipke(m,tol); 
+
 zeta = 1 - z./E1;
 r = sqrt(zeta.*zeta+mu.*mu); 
-theta = atan(mu./(z+1.E-12));
-invE = pi/2 + sqrt(r).*(theta - (pi/2)); 
+theta = atan(mu./(z+eps));
+
+% “Empirical” initialization [1]
+invE(:) = pi/2 + sqrt(r).*(theta - (pi/2)); 
 
 for iter=1:4
-    [F, E] = elliptic12(invE,m);
-    invE = invE-(E - z)./sqrt(1-m.*sin(invE).^2 ); 
+    [~, E] = elliptic12(invE(:),m,tol);
+    invE(:) = invE(:)-(E - z)./sqrt( 1-m.*sin(invE(:)).^2 ); 
 end
 return;
 
